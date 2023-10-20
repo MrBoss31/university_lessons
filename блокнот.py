3bab171788926b8e3b375a4433867dc72f962776
Mr. BOSS <anarbek@mail.ru>
2023-10-20 17:33:30 +0600

    second commit

---
+++ блокнот.py
@@ -0,0 +1,7 @@
+file_name = "myfile.txt"  # Specify the file name
+text_to_write = "Привет, !\nЭто вторая строка.\nИ ещё одна строка."
+
+with open(file_name, "w", encoding="UTF-8") as file:
+    file.write(text_to_write)
+
+print(f"Файл {file_name} успешно создан и записан.")
